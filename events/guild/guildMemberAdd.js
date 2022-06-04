785a0714811871d1882805d727caefef872f8642
fbarrena <fbarrena@dc.uba.ar>
2022-06-04 16:56:49 -0300

    --no-comment

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const welcomes = ['Welcome to the Server! Before you can start enjoying our marvelous community you will have to answer just one question!\nWould you rather eat a baby goat or a matter baby?', 'who are you identify yourself\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nplease :)'];
 module.exports = (client, Discord, guildMember) => {
-   // const welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'citizens');
-	//guildMember.roles.add(welcomeRole);
+    const welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'citizens');
+	guildMember.roles.add(welcomeRole);
     const channel = guildMember.guild.channels.cache.get(process.env.general);
     if (!channel) return;
     const randomIndex = Math.floor(Math.random() * welcomes.length);
