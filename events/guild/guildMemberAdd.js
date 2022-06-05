34ff280e96f742b9d4ebd9ef80c57414411430d0
sudobarre <fedebarrena@gmail.com>
2022-06-05 09:19:57 -0300

    --no-comment

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const welcomes = ['Welcome to the Server! Before you can start enjoying our marvelous community you will have to answer just one question!\nWould you rather eat a baby goat or a matter baby?', 'who are you identify yourself\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nplease :)'];
 module.exports = (client, Discord, guildMember) => {
-    const welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'citizens');
+    const welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Citizen of Nothingham');
 	guildMember.roles.add(welcomeRole);
     const channel = guildMember.guild.channels.cache.get(process.env.general);
     if (!channel) return;
