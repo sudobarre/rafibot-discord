5c8d140b90b2f3ee2e13d023ffc39fdce0eaf062
sudobarre <fedebarrena@gmail.com>
2022-06-11 15:17:06 -0300

    yolo

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -36,7 +36,7 @@ module.exports = (client, Discord, message) => {
     }*/
     switch (message.author.id) {
       case process.env.devila:
-        message.reply('bitch');
+        message.reply('bimp');
         break;
       case process.env.sima:
         if (
