e051fc4fa0243b1b8154b0f5184d8e0ef22bdc9a
sudobarre <fedebarrena@gmail.com>
2022-06-12 13:03:24 -0300

    bot is now a snitch

--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,6 +1,6 @@
 module.exports = (client, Discord, member) => {
      const channel = member.guild.channels.cache.get(process.env.general);
      if (!channel) return;
-     channel.send('A member left the server.');
+     channel.send(`Goodbye ${member.tag}, we are sorry to see you go!`);
  };
  
