4a265173c27427a95b42a4df4876e7554e369477
sudobarre <fedebarrena@gmail.com>
2022-06-19 14:38:23 -0300

    only adds role to nothingham

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,6 @@
 const welcomes = ['Welcome to the Server! Before you can start enjoying our marvelous community you will have to answer just one question!\nWould you rather eat a baby goat or a matter baby?'];
 module.exports = (client, Discord, guildMember) => {
+    if(guildMember.guild.id != process.env.guildId) return;
     const welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Citizen of Nothingham');
 	guildMember.roles.add(welcomeRole);
     const channel = guildMember.guild.channels.cache.get(process.env.general);
