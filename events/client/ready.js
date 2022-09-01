825cab88142ec1e54b79a15fcf5d3c91f0a8a00e
fbarrena <fbarrena@dc.uba.ar>
2022-08-31 22:39:06 -0300

    minor fix

--- events/client/ready.js
+++ events/client/ready.js
@@ -19,8 +19,8 @@ module.exports = (client) =>{
         }
         //send it via DM to users
         getQuote().then(quote => {
-				const nothingham = await client.guilds.cache.get(process.env.guildId);
-				const inspiration = await nothingham.channels.cache.get(process.env.inspiration);
+				const nothingham = client.guilds.cache.get(process.env.guildId);
+				const inspiration = nothingham.channels.cache.get(process.env.inspiration);
 				client.users.send(process.env.jessil, quote);
 				inspiration.send(quote);
 
